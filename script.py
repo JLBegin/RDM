cb51c9cb92ed5cccf5ba9f54b2e60f4a86875a1b
Ludovick Bégin <ludovick.begin@gmail.com>
2019-04-01 21:17:53 -0400

    Update script.py

--- script.py
+++ script.py
@@ -13,78 +13,104 @@ Trouver la contrainte dans les cas suivant:
 
 class AirPlane:
     def __init__(self):
+        self.chargeFactor = 1
+        self.onGround = True
         self.resolution = 1000
 
         self.wingLength = 150
         self.totalWeight = 0
-        self.apparentWeight = 10
         self.nervures = np.array([[25, 50, 80.8, 111.5, 150], [7.13, 5.89, 6.86, 3.35, 3.21]])
         self.wheel = [25, 110]
         self.bl = 1.648
         self.tl = 0.16
 
-        self.chargeFactor = 1
-        self.gravity = 386.0886
         self.pAlum = 0.101
         self.pFuel = 0.0303
+        self.beamHeightFunc = None
         self.beamAreaFunc = None
         self.shear = []
+        self.moment = [0]
 
     def getWingShearAndMoment(self):
+        if self.onGround:
+            self.nervures[1][0] -= 1815.96
         self.getTotalWeight()
-        self.getApparentWeight()
 
         A = np.linspace(150, 0, self.resolution)[1:]
 
+        """Calcul de l'effort tranchant"""
+
         for i, a in enumerate(A):
-            shear = self.getWingSectionWeight(a) - self.getAeroLoad(a)
-            self.shear.append(shear * 4.44822 / 1000)
+            if self.onGround:
+                shear = self.getWingSectionWeight(a)
+            else:
+                shear = self.getWingSectionWeight(a) - self.getAeroLoad(a)
+                shear *= self.chargeFactor
 
-        fig, (ax1, ax2) = plt.subplots(2)
+            self.shear.append(-1*shear)
+
+        fig, (ax1, ax2) = plt.subplots(2, figsize=(7, 6))
         ax1.plot(A, self.shear)
-        ax1.set_ylabel("Force [kN]")
-        ax1.set_xlabel("Distance sur l'aile [pouce]")
+        ax1.set_ylabel("Effort Tranchant [lb]", fontsize=13)
 
-        X, self.shear = list(reversed(A)), list(reversed(self.shear))
+        """Calcul du moment"""
+
+        X = A
+        # X = list(reversed(A))
+        # self.shear = list(reversed(self.shear))
         Xstep = 150 / (self.resolution-1)
 
-        M = [0]
         for y in self.shear:
-            M.append(y*Xstep + M[-1])
+            self.moment.append(y*Xstep + self.moment[-1])
 
-        ax2.plot(X, M[1:])
-        ax2.set_ylabel("Moment")
+        ax2.plot(X, self.moment[1:])
+        ax2.set_ylabel("Moment [lb$\\times$in]", fontsize=13)
+
+        ax2.set_xlabel("Distance sur l'aile [pouce]", fontsize=13)
 
         plt.show()
 
     def getBeamStrain(self):
-        X = np.linspace(0, 150, self.resolution)[1:]
-
-        areas = []
+        X = np.linspace(150, 0, self.resolution)[1:]
         x = symbols("x")
+
+        inertiaFunc = (0.0018394 * 0.125 * (((-20.6/150) * x) + 41.2)**3) + 2*(((1/12) * self.bl * (self.beamHeightFunc**3)) - ((1/12) * (self.bl - self.tl) * (self.beamHeightFunc - 2*self.tl)**3))
+        QFunc = ((self.bl * self.tl * (self.beamHeightFunc/2 + self.tl/2)) + (self.tl * (self.beamHeightFunc/2 - self.tl) * ((self.beamHeightFunc/2 - self.tl/2)/2))) * 2
+
+        Q = []
+        height = []
+        inertia = []
         for d in X:
-            areas.append(self.beamAreaFunc.evalf(subs={x: d}))
+            Q.append(QFunc.evalf(subs={x: d}))
+            height.append(self.beamHeightFunc.evalf(subs={x: d}))
+            inertia.append(inertiaFunc.evalf(subs={x: d}))
 
-        strain = np.array(self.shear) / (2 * np.array(areas))
+        normalStrain = ((np.array(self.moment[1:]) * (np.array(height)/2)) / (np.array(inertia))) / 1000
+        shearStrain = ((np.array(self.shear) * np.array(Q)) / (np.array(inertia) * self.tl)) / 1000
 
-        plt.plot(X, strain)
+        print("Max Normal Strain = {} || SF = {}".format(round(np.max(normalStrain), 3), round(60/np.max(normalStrain), 3)))
+        print("Max Shear  Strain = {} || SF = {}".format(round(np.max(shearStrain), 3), round(25/np.max(shearStrain), 3)))
+
+        fig, (ax1, ax2) = plt.subplots(2, figsize=(7, 6))
+        ax1.plot(X, shearStrain)
+        ax2.plot(X, normalStrain)
+        ax2.set_ylabel("Contrainte\nnormale [ksi]", fontsize=13)
+        ax1.set_ylabel("Contrainte\nde cisaillement [ksi]", fontsize=13)
+        ax2.set_xlabel("Distance sur l'aile [pouce]", fontsize=13)
         plt.show()
 
     def getTotalWeight(self):
-        mainWeight = 5200 * self.gravity
-        self.totalWeight = 2 * self.getWingSectionWeight() + mainWeight
-
-    def getApparentWeight(self):
-        self.apparentWeight = self.totalWeight * self.chargeFactor
+        mainWeight = 5200
+        wingWeight = self.getWingSectionWeight()
+        self.totalWeight = 2 * wingWeight + mainWeight
+        print("wing/Total = {} / {}".format(wingWeight, self.totalWeight))
 
     def getWingSectionWeight(self, a=0, b=150):
-        fuelWeight = self.getFuelVolume() * self.gravity * self.pFuel
-        beamWeight = 2 * self.getBeamVolume() * self.gravity * self.pAlum
-        coatWeight = self.getCoatingWeight(a, b)
-        nervWeight = sum(self.nervures[1][np.where((self.nervures[0] >= a) & (self.nervures[0] <= b))[0]]) * self.gravity
-        wheelWeight = self.wheel[1] * self.gravity if a <= self.wheel[0] else 0
-
-        # print([fuelWeight, beamWeight, coatWeight, nervWeight, wheelWeight])
+        fuelWeight = self.getFuelVolume(a, b) * self.pFuel
+        beamWeight = 2 * self.getBeamVolume(a, b) * self.pAlum
+        coatWeight = self.getCoatingVolume(a, b) * self.pAlum
+        nervWeight = sum(self.nervures[1][np.where((self.nervures[0] >= a) & (self.nervures[0] <= b))[0]])
+        wheelWeight = self.wheel[1] if a <= self.wheel[0] else 0
 
         weight = sum([fuelWeight, beamWeight, coatWeight, nervWeight, wheelWeight])
 
@@ -92,8 +118,9 @@ class AirPlane:
 
     def getFuelVolume(self, a=0, b=150):
         x = symbols('x')
-        beamDistance = 14.4 - 4.8 * x * 10**-2
-        beamHeight = 3.296 - 1.08967 * x * 10**-2
+        beamDistance = (((7.2-14.4)/150) * x) + 14.4
+        beamHeight = (((1.648-3.256)/150) * x) + 3.256
+
         beamAreaFunc = ((beamHeight-2*self.tl)*self.tl)+2*self.bl*self.tl
 
         areaFunc = beamDistance * beamHeight - 2*beamAreaFunc
@@ -105,8 +132,8 @@ class AirPlane:
 
     def getBeamVolume(self, a=0, b=150):
         x = symbols('x')
-        beamHeight = 3.296 - 1.08967 * x * 10**-2
-        self.beamAreaFunc = ((beamHeight-2*self.tl)*self.tl)+2*self.bl*self.tl
+        self.beamHeightFunc = (((1.648-3.256)/150) * x) + 3.256
+        self.beamAreaFunc = ((self.beamHeightFunc-2*self.tl)*self.tl)+2*self.bl*self.tl
 
         beamVolume = integrate(self.beamAreaFunc, (x, a, b))
         # meanX = integrate(x*beamAreaFunc, (x, a, b)) / beamVolume
@@ -115,20 +142,21 @@ class AirPlane:
 
     def getAeroLoad(self, a=0, b=150):
         x = symbols('x')
-        liftFunc = 9 * self.apparentWeight / (16 * self.wingLength) * (1 - (x / self.wingLength)**8)
+        liftFunc = ((9 * self.totalWeight) / (16 * self.wingLength)) * (1 - (x / self.wingLength)**8)# fonction d prof
         aeroLoad = integrate(liftFunc, (x, a, b))
         # meanX = integrate(x*liftFunc, (x, a, b)) / aeroLoad
 
         return aeroLoad  # , meanX
 
-    def getCoatingWeight(self, a=0, b=150):
+    @staticmethod
+    def getCoatingVolume(a=0, b=150):
         x = symbols('x')
-        weightFunc = 2.004 * 0.125 * self.pAlum * self.gravity * ((-20.6*x/150) + 41.2)
+        areaFunc = 2.024 * 0.125 * ((-20.6*x/150) + 41.2)
 
-        coatWeight = integrate(weightFunc, (x, a, b))
-        # meanX = integrate(x*weightFunc, (x, a, b)) / coatWeight
+        coatVolume = integrate(areaFunc, (x, a, b))
+        # meanX = integrate(x*areaFunc, (x, a, b)) / coatVolume
 
-        return coatWeight  # , meanX
+        return coatVolume  # , meanX
 
 
 plane = AirPlane()
